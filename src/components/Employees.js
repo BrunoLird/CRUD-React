190c92a2998af1eb87cd7248bc41c625544ad943
Bruno Lird <brunolird@gmail.com>
2023-01-16 20:39:19 -0300

    first commit

---
+++ src/components/Employees.js
@@ -0,0 +1,17 @@
+const Employees = [
+{
+    id:"1",
+    Name:"Seba Rios",
+    Age: "20"
+},{
+    id:"2",
+    Name:"Joseca",
+    Age: "30"
+},{
+    id:"3",
+    Name:"Wuanaco",
+    Age: "25"
+}
+]
+
+export default Employees;
